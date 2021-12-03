f92da8a81b2ab3e6fe568e0405e90dac1d64de18
AgusZ02 <76200509+AgusZ02@users.noreply.github.com>
2021-12-03 22:10:50 +0100

    Update ProgramaPrincipal.java

--- src/ProgramaPrincipal.java
+++ src/ProgramaPrincipal.java
@@ -5,7 +5,8 @@ public class ProgramaPrincipal {
 	public static void main(String[] args) {
 		String rutaFichero = "TrackFiles\\Athlete1\\activity-Athlete1-01.csv";
 		System.out.println("Hello world");
-		// Hola
+		InfoLogTrack fichero = FuncionalidadAuxiliar.cargarInfoCSV(rutaFichero);
+		System.out.println(Arrays.toString(fichero.frecCardiaca));
 	}
 	
 }
