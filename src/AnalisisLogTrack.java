246beb5dd24d888ede045b94c91ca65c1e8cb823
AgusZ02 <76200509+AgusZ02@users.noreply.github.com>
2021-12-08 17:15:41 +0100

    Distancia entre puntos y velocidad terminado

--- src/AnalisisLogTrack.java
+++ src/AnalisisLogTrack.java
@@ -19,7 +19,10 @@ public class AnalisisLogTrack {
 		}
 		BasicStats.distancia = DistanciaEntrePuntos(pInfo.latitud, pInfo.longitud);
 		BasicStats.fCMedia = sumatorio / pInfo.frecCardiaca.length;
+
 		BasicStats.duracion = pInfo.tiempo[pInfo.tiempo.length -1];
+
+
 		BasicStats.velocidad = (BasicStats.distancia / ((double) BasicStats.duracion / 3600) );
 		return BasicStats;
 	}
@@ -74,29 +77,33 @@ public class AnalisisLogTrack {
 	 */
 	public static void generarInformesTrack(InfoLogTrack pInfo) {
 		EstadisticasBasicas Informe = obtEstadisticasBasicas(pInfo);
+		int segundosTotales = Informe.duracion;
+		int horas = segundosTotales / 3600;
+		int minutos = (segundosTotales % 3600) / 60;
+		int segundos = segundosTotales % 60;
+		String tiempo = String.format("%d:%d:%d", horas,minutos,segundos);
 		System.out.println("Frecuendia cardíaca media: " + Informe.fCMedia + " pulsaciones por minuto.");
-		System.out.println("Duracion de la actividad: " + Informe.duracion + " segundos");
+		System.out.println("Duracion de la actividad: " + tiempo);
 		System.out.println("Distancia total recorrida: " + Informe.distancia + " Kilometros");
 		System.out.println("Velocidad media del atleta: " + Informe.velocidad + " Kilometros por hora");
+
+
 	}
 
 
 	public static float DistanciaEntrePuntos(double[] lat, double[] longi){
-		//float distancia;
-		//distancia = 0;
-		float Dlat = Math.abs((float) (lat[lat.length-1] - lat[0]));
-		float Dlong = Math.abs((float) (longi[longi.length-1] - longi[0]));
-		float a = (float) ((float) Math.pow(Math.sin(Math.toRadians(Dlat)), 2) + Math.cos(Math.toRadians(lat[0])) * Math.cos(Math.toRadians(lat[lat.length-1])) * Math.sin(Math.toRadians(Dlong)));
-		float d = (float) (2 * AnalisisLogTrack.RADIO_TIERRA_KM * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)));
-		return d;
-		//for (int i = 0; i < lat.length -1; i++) {
-		//	float Dlat = Math.abs((float) (lat[i+1] - lat[i]));
-		//	float Dlong = Math.abs((float) (longi[i+1] - longi[i]));
-		//	float a = (float) ((float) Math.pow(Math.sin(Math.toRadians(Dlat)), 2) + Math.cos(Math.toRadians(lat[i])) * Math.cos(Math.toRadians(lat[i+1])) * Math.sin(Math.toRadians(Dlong)));
-		//	float d = (float) (2 * AnalisisLogTrack.RADIO_TIERRA_KM * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)));
-		//	distancia += d;
+		float distancia;
+		distancia = 0;
+		for (int i = 0; i < lat.length -1; i++) {
+			float Dlat = Math.abs((float) (lat[i+1] - lat[i]));
+			float Dlong = Math.abs((float) (longi[i+1] - longi[i]));
+			Dlat = (float) Math.toRadians(Dlat);
+			Dlong = (float) Math.toRadians(Dlong);
+			float a = (float) ((float) Math.pow(Math.sin(Dlat)/2, 2) + Math.cos(Math.toRadians(lat[i])) * Math.cos(Math.toRadians(lat[i+1])) * Math.pow(Math.sin(Dlong/2),2));
+			float d = (float) (2 * AnalisisLogTrack.RADIO_TIERRA_KM * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)));
+			distancia += d;
 
-		//}
-		//return distancia;
+		}
+		return distancia;
 	}
 }
--- src/ProgramaPrincipal.java
+++ src/ProgramaPrincipal.java
@@ -3,43 +3,43 @@ import java.util.Scanner;
 
 public class ProgramaPrincipal {
 
-	public static String pregunta(String enunciado) {
+	public static int pregunta(String enunciado) {
 		Scanner escaner = new Scanner(System.in);
 		System.out.println(enunciado);
-		String respuesta = escaner.nextLine();
+		int respuesta = escaner.nextInt();
 		return respuesta;
 	}
 
 	public static void main(String[] args) {
 		String ruta = "";
-		String respuesta = pregunta("De qué atleta quieres la informacion? (1-7)");
-		String respuestaFichero = "";
+		int respuesta = pregunta("De qué atleta quieres la informacion? (1-7)");
+		int respuestaFichero = 0;
 		switch (respuesta) {
-			case "1":
+			case 1:
 				respuestaFichero = pregunta("Qué actividad quieres leer? (1-5)");
 				ruta = FunSwitch(1, Integer.valueOf(respuestaFichero));
 				break;
-			case "2":
+			case 2:
 				respuestaFichero = pregunta("Qué actividad quieres leer? (1-5)");
 				ruta = FunSwitch(2, Integer.valueOf(respuestaFichero));
 				break;
-			case "3":
+			case 3:
 				respuestaFichero = pregunta("Qué actividad quieres leer? (1-5)");
 				ruta = FunSwitch(3, Integer.valueOf(respuestaFichero));
 				break;
-			case "4":
+			case 4:
 				respuestaFichero = pregunta("Qué actividad quieres leer? (1-5)");
 				ruta = FunSwitch(4, Integer.valueOf(respuestaFichero));
 				break;
-			case "5":
+			case 5:
 				respuestaFichero = pregunta("Qué actividad quieres leer? (1-5)");
 				ruta = FunSwitch(5, Integer.valueOf(respuestaFichero));
 				break;
-			case "6":
+			case 6:
 				respuestaFichero = pregunta("Qué actividad quieres leer? (1-5)");
 				ruta = FunSwitch(6, Integer.valueOf(respuestaFichero));
 				break;
-			case "7":
+			case 7:
 				respuestaFichero = pregunta("Qué actividad quieres leer? (1-5)");
 				ruta = FunSwitch(7, Integer.valueOf(respuestaFichero));
 				break;
@@ -50,7 +50,7 @@ public class ProgramaPrincipal {
 		AnalisisLogTrack.DistanciaEntrePuntos(atletaTrackpoints.latitud, atletaTrackpoints.longitud);
 }
 	public static String FunSwitch(int Atleta, int Actividad){
-		String rutaO = "TrackFiles\\AthleteN\\activity-AthleteN-JJ.csv";
+		String rutaO = String.format("TrackFiles\\Athlete%d\\activity-Athlete%d-%2d.csv", Atleta, Actividad);
 		String rutaR = "";
 		String ruta = rutaO.replace("N", Integer.toString(Atleta));
 
