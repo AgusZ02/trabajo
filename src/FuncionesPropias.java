7aa578dc75fa7b6feb973cf9ceaeed1cf13d2b79
AgusZ02 <76200509+AgusZ02@users.noreply.github.com>
2021-12-25 21:33:33 +0100

    Update FuncionesPropias.java

--- src/FuncionesPropias.java
+++ src/FuncionesPropias.java
@@ -54,12 +54,13 @@ public class FuncionesPropias {
     }
     public static double CalculoMedia(int[] array){
         double sumatorio;
+        double media;
         sumatorio = 0;
         for (int i = 0; i < array.length; i++) {
             sumatorio += array[i];
 
         }
-        double media = sumatorio/array.length;
+        media = sumatorio/array.length;
         return media;
     }
 }
